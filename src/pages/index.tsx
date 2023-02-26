db251ce80e608c7848fdad266f48885414da016e
Anh.Tuan <nttanh6299@gmail.com>
2023-02-26 21:55:30 +0700

    Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,123 +1,274 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { animate, motion, useMotionValue, useWillChange } from "framer-motion";
 
-const inter = Inter({ subsets: ['latin'] })
+interface Pokemon {
+  id: string;
+  name: string;
+  dropPercent: number;
+  rarity: string;
+  image: string;
+}
+
+const pokemon: Pokemon[] = [
+  {
+    id: "1",
+    name: "Bulbasaur",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    id: "2",
+    name: "Ivysaur",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+  {
+    id: "3",
+    name: "Venusaur",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+  },
+  {
+    id: "4",
+    name: "Charmander",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    id: "5",
+    name: "Zigzagoon Galar",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10174.png",
+  },
+  {
+    id: "6",
+    name: "Linoone Galar",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10175.png",
+  },
+  {
+    id: "7",
+    name: "Raikou",
+    dropPercent: 79.92,
+    rarity: "rare",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/243.png",
+  },
+  {
+    id: "8",
+    name: "Entei",
+    dropPercent: 15.98,
+    rarity: "mythical",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/244.png",
+  },
+  {
+    id: "9",
+    name: "Larvitar",
+    dropPercent: 15.98,
+    rarity: "mythical",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/246.png",
+  },
+  {
+    id: "10",
+    name: "Pupitar",
+    dropPercent: 15.98,
+    rarity: "mythical",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/247.png",
+  },
+  {
+    id: "11",
+    name: "Tiranitar",
+    dropPercent: 15.98,
+    rarity: "mythical",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/248.png",
+  },
+  {
+    id: "12",
+    name: "Lugia",
+    dropPercent: 15.98,
+    rarity: "mythical",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png",
+  },
+  {
+    id: "13",
+    name: "Salandit",
+    dropPercent: 3.2,
+    rarity: "legendary",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/757.png",
+  },
+  {
+    id: "14",
+    name: "Salazzle",
+    dropPercent: 3.2,
+    rarity: "legendary",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/758.png",
+  },
+  {
+    id: "15",
+    name: "Iron Valiant",
+    dropPercent: 3.2,
+    rarity: "legendary",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1006.png",
+  },
+  {
+    id: "16",
+    name: "Joltik",
+    dropPercent: 0.64,
+    rarity: "ancient",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/595.png",
+  },
+  {
+    id: "17",
+    name: "Maschiff",
+    dropPercent: 0.64,
+    rarity: "ancient",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/942.png",
+  },
+  {
+    id: "18",
+    name: "Mabossiff",
+    dropPercent: 0.26,
+    rarity: "spiritual",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/943.png",
+  },
+];
+
+const drop = (items: Pokemon[]) => {
+  let chances = [];
+
+  const sum = items.reduce((prev, curr) => prev + curr.dropPercent, 0);
+
+  let acc = 0;
+  chances = items.map(({ dropPercent }) => (acc = dropPercent + acc));
+
+  const rand = Math.random() * sum;
+
+  const itemIndex = chances.filter((el) => el <= rand).length;
+
+  const result = items.find((_, index) => index === itemIndex);
+
+  return result;
+};
+
+function random(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffle(array: any[]) {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
 
 export default function Home() {
+  const x = useMotionValue(0);
+  const willChange = useWillChange();
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon>();
+  const [spinItems, setSpinItems] = useState<Pokemon[]>([]);
+  const [spinning, setSpinning] = useState(false);
+
+  const onClick = () => {
+    if (spinning) return;
+
+    setSpinning(true);
+
+    const selected = drop(pokemon);
+
+    const shuffleItems = shuffle([
+      ...pokemon,
+      ...pokemon,
+      ...pokemon,
+      ...pokemon,
+      ...pokemon,
+    ]);
+
+    shuffleItems[83] = selected;
+
+    setSpinItems(shuffleItems);
+
+    animate(x, random(-14980, -15140), {
+      ease: [0.2, 0, 0.6, 1],
+      duration: 2,
+      onComplete: () => {
+        setTimeout(() => {
+          setSelectedPokemon(selected);
+          setSpinning(false);
+          x.set(0);
+        }, 8500);
+      },
+    });
+  };
+
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+    <div className={styles.container}>
+      {pokemon.map((pokemon, index) => (
+        <div key={pokemon.id} className={styles.pokemon}>
+          <div className={styles.image}>
+            <img src={pokemon.image} width="180px" height="150px" />
+          </div>
+          <div
+            className={`${styles.text} ${
+              styles[pokemon.rarity] || styles.default
+            }`}
+          >
+            {pokemon.name} {index}
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+      ))}
+      <div className={styles.list}>
+        <div
+          className={styles.holder}
+          style={{ display: spinning ? "block" : "none" }}
+        >
+          <motion.div style={{ x, willChange }} className={styles.roller}>
+            {spinItems.map((pokemon, index) => (
+              <div key={pokemon.id} className={styles.pokemon}>
+                <div className={styles.image}>
+                  <img src={pokemon.image} width="180px" height="150px" />
+                </div>
+                <div
+                  className={`${styles.text} ${
+                    styles[pokemon.rarity] || styles.default
+                  }`}
+                >
+                  {pokemon.name} {index}
+                </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
-  )
+      </div>
+      <button onClick={onClick}>Open</button>
+      <div>You get: {selectedPokemon?.name}</div>
+    </div>
+  );
 }
